988a6ef109bfba911100277b2b5e5050b9667c27
AlexGnhrov <gonharov.alex@ya.ru>
2021-09-21 13:28:52 +0300

    Работа 21

---
+++ PR4/NAME/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PR4
+{
+    class Person
+    {
+        public string name = "Саня";
+        public int age = 17;
+
+        public void GetInfo()
+        {
+            System.Console.WriteLine($"name: {name} age: {age}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Person Alex = new Person();
+            Alex.GetInfo();
+            System.Console.ReadKey();
+
+            Alex.name = "Александр";
+            Alex.age = 17;
+            Alex.GetInfo();
+            System.Console.ReadKey();
+        }
+    }
+}
